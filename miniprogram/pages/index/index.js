06d193461dbb19a36ff2f567bdee00efeef31427
Voyz <ghshen@trip.com>
2021-06-24 16:34:10 +0800

    [dev]首页样式更新

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,22 +4,22 @@ import {getUserDeviceInfo} from '../../api/index'
 
 Page({
     data: {
-        
+        sceneList:[]
     },
     onLoad: function () {
-        // 获取用户openid
+        const _this = this;
         wx.login({
             success: function (res) {
                 const openid = res.code;
                 app.openid = openid;
                 getUserDeviceInfo(openid).then(e=>{
-                    console.log(e);
+                    _this.setData(e)
+                    console.log(e.sceneList);
                 })
             }
         });
     },  
     // 切换标签
     onChange: function (e) {
-        console.log(app.openid);
     }
 });
