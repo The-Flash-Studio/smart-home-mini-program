7f81671322a2f2ea9a51ff7ba19954bb57d217fb
chuangma <chuangma@trip.com>
2021-07-27 15:36:35 +0800

    change icon

--- miniprogram/api/devcieApi.js
+++ miniprogram/api/devcieApi.js
@@ -54,6 +54,7 @@ export function getAllMockDevices() {
           name: "回家开灯",
           description: "回家自动开灯，打开空调",
           sceneType: 1,
+          sceneIcon:"bulb-o",
           equipmentList: [{
               name: '智能空调',
               position: '客厅',
@@ -72,6 +73,7 @@ export function getAllMockDevices() {
           name: "我出门了",
           description: "关灯并打开扫地机，打开扫地机，关灯和窗帘，空调调至睡眠模式关",
           sceneType: 2,
+          sceneIcon:"wap-home-o",
           equipmentList: [{
               name: '智能空调',
               position: '客厅',
@@ -91,6 +93,7 @@ export function getAllMockDevices() {
           description: "关灯和窗帘，空调调至睡眠模式关灯和窗帘",
           hasSet: true,
           sceneType: 3,
+          sceneIcon:"closed-eye",
           equipmentList: [{
               name: '智能空调',
               position: '客厅',
@@ -112,6 +115,7 @@ export function getAllMockDevices() {
       sceneList: [{
         name: "碰碰贴的智能玩法",
         description: "一碰尽享智能联动",
+        sceneIcon:"coupon-o",
         sceneType: 4,
       }, ]
     },
